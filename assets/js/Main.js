d5e4f4ea937b391110165486fe272964b5534cf1
Anant Anand Gputa <anantanandgupta@gmail.com>
2017-03-20 00:33:10 +0530

    scroll issue and swipe detection fix

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -1,1 +1,1 @@
-define(["require","exports","./GameControl"],function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=document.getElementById("btnReset");document.body.addEventListener("touchmove",function(e){e.preventDefault()},!1),n.Start("game-tile"),o.onclick=function(){n.reset()}});
+define(["require","exports","./GameControl"],function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=document.getElementById("btnReset");n.Start("game-tile"),r.onclick=function(){n.reset()}});
